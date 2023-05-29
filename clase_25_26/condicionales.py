35a0cfc2cf4c960f1434279efafa633b6b0e7c6b
Anderson Ocaña <andru.ocatorres@gmail.com>
2023-05-29 23:28:24 +0000

    condicionales

---
+++ clase_25_26/condicionales.py
@@ -0,0 +1,6 @@
+# if - elif - else
+
+# siempre tienen esa funcionalidad de que en cualquier lenguaje de programacion
+# donde te permite seleccionar el recorrido de la decision requerida
+
+# EJEMPLO PRACTICO
