bc9bb68886f9941787e8bb753b3da5e5ad9d985c
andru-oca <112781394+andru-oca@users.noreply.github.com>
2023-06-08 19:11:57 +0000

    class updated

--- clase_29/package_manager/Manager.py
+++ clase_29/package_manager/Manager.py
@@ -5,12 +5,13 @@ from uuid import uuid4
 
 class Beer():
     def __init__(self,**kwargs):
-        self.beer_id : str(uuid4()) 
-        self.nombre : kwargs["nombre"],
-        self.abv : kwargs["abv"],
-        self.url : kwargs["url"]
+        id = uuid4()
+        self.beer_id = str(id)
+        self.nombre = kwargs["nombre"]
+        self.abv = kwargs["abv"]
+        self.url = kwargs["url"]
 
-    def __dict__(self):
+    def dict(self):
         return {
         "beer_id":self.beer_id,
         "nombre":self.nombre,
@@ -28,7 +29,21 @@ class Manager():
             data = json.load(db)
             return data
     def db_create(self, beer:Beer):
-     
+        data = self.db_reader()
+        data.append(beer)
+        with open(self.path, "w") as db:
+            json.dump(data,db,indent=4)
+
+    def db_update(self,beer:Beer):
+        pass
+    
+    def db_delete(self,beer:Beer):
+        pass
+        
+    def db_search_one(self,beer_id:Beer):
+        pass
+
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 path = "./db/beer_db.json"
 
@@ -39,7 +54,11 @@ beer_manager = Manager(file)
 print(beer_manager.db_reader())
 
 beer_insert = Beer(
-    
+    nombre = "ginger ale",
+    abv = 14 ,
+    url = "https://www.acouplecooks.com/wp-content/uploads/2020/08/Ginger-Beer-001.jpg"
 )
 
+
+beer_manager.db_create(beer_insert.dict())
 
